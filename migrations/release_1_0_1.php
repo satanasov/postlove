5ace89555811aac908961bf1f96dd343b7592f71
Stanislav Atanasov <lucifer@anavaro.com>
2014-08-12 11:16:33 +0300

    Add migration for switching CSS off

---
+++ migrations/release_1_0_1.php
@@ -0,0 +1,33 @@
+<?php
+/**
+*
+* Post Love extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2014 Lucifer <http://www.anavaro.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace anavaro\postlove\migrations;
+
+/**
+* Primary migration
+*/
+
+class release_1_0_1 extends \phpbb\db\migration\migration
+{
+	static public function depends_on()
+	{
+		return array(
+			'\anavaro\postlove\migrations\release_1_0_0',
+		);
+	}
+	
+	public function update_data()
+	{
+		return array(
+			array('config.add', array('postlove_use_css', '1')),
+		);
+	}
+
+}
