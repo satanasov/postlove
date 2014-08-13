bad09be4302cddd6b4194dda71a8f3b11f1a9510
Stanislav Atanasov <lucifer@anavaro.com>
2014-08-13 07:29:36 +0300

    Adding tests

---
+++ tests/functional/postlove_acp_test.php
@@ -0,0 +1,39 @@
+<?php
+/**
+*
+* Birthday Control test
+*
+* @copyright (c) 2014 Stanislav Atanasov
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace anavaro\postlove\tests\functional;
+
+/**
+* @group functional
+*/
+class postlove_acp_test extends postlove_base
+{
+	public function acp_pages_data()
+	{
+		return array(
+			array('acp_board&mode=features'), // Load the advanced forum settings ACP page
+		);
+	}
+	/**
+	* @dataProvider acp_pages_data
+	*/
+	public function test_acp_pages($mode)
+	{
+		$this->login();
+		$this->admin_login();
+
+		$this->add_lang_ext('anavaro/postlove', 'info_acp_birthdaycontrol');
+
+		$crawler = self::request('GET', 'adm/index.php?i=' . $mode . '&sid=' . $this->sid);
+		$this->assertContainsLang('POSTLOVE_USE_CSS', $crawler->text());
+		$this->assertContainsLang('POSTLOVE_SHOW_LIKES', $crawler->text());
+		$this->assertContainsLang('POSTLOVE_SHOW_LIKED', $crawler->text());
+	}
+}
---
+++ tests/functional/postlove_base.php
@@ -0,0 +1,75 @@
+<?php
+/**
+*
+* Birthday Control
+*
+* @copyright (c) 2014 Stanislav Atanasov
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace anavaro\postlove\tests\functional;
+
+/**
+* @group functional
+*/
+class postlove_base extends \phpbb_functional_test_case
+{
+	static protected function setup_extensions()
+	{
+		return array('anavaro/postlove');
+	}
+
+	public function setUp()
+	{
+		parent::setUp();
+		$this->force_allow_postlove();
+	}
+
+	/**
+	* Allow birthday (just to be sure) 
+	*/
+	public function force_allow_postlove()
+	{
+		$this->get_db();
+
+		$sql = "UPDATE phpbb_config
+			SET config_value = 1
+			WHERE config_name = 'postlove_use_css'";
+
+		$this->db->sql_query($sql);
+
+		$this->purge_cache();
+	}
+
+	/**
+	* Require birthday (it's not required on install) 
+	*/
+	public function show_likes()
+	{
+		$this->get_db();
+
+		$sql = "UPDATE phpbb_config
+			SET config_value = 1
+			WHERE config_name = 'postlove_show_likes'";
+
+		$this->db->sql_query($sql);
+
+		$this->purge_cache();
+	}
+	/**
+	* Set age (default is 0)
+	*/
+	public function show_liked()
+	{
+		$this->get_db();
+
+		$sql = "UPDATE phpbb_config
+			SET config_value = 1
+			WHERE config_name = 'postlove_show_liked'";
+
+		$this->db->sql_query($sql);
+
+		$this->purge_cache();
+	}
+}
