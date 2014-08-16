ac5a2a5bab6ffad86371e6538dd5567cc953dff6
phpBB España <webmaster@phpbb-es.com>
2014-08-16 23:45:29 +0200

    Update info_acp_postlove.php
    Small change...

--- language/es/info_acp_postlove.php
+++ language/es/info_acp_postlove.php
@@ -22,7 +22,7 @@ if (empty($lang) || !is_array($lang))
 $lang = array_merge($lang, array(
 	'POSTLOVE_CONTROL'	=> 'Mensaje que gusta',
 	'POSTLOVE_USE_CSS'	=> 'Usar CSS proporcionado',
-	'POSTLOVE_USE_CSS_EXPLAIN'	=> 'Para facilitar la personalización de la extensión POST LOVE, podría usted evitar la carga del CSS por defecto. Si desea utilizar sus propias imágenes, por favor, consulte el archivo <code>overall_header_head_append.html</code>',
+	'POSTLOVE_USE_CSS_EXPLAIN'	=> 'Para facilitar la personalización de la extensión POST LOVE, podría evitar la carga del CSS por defecto. Si desea utilizar sus propias imágenes, por favor, consulte el archivo <code>overall_header_head_append.html</code>',
 	'POSTLOVE_SHOW_LIKES'	=> 'Mostrar el número de mensajes que le han gustado.',
 	'POSTLOVE_SHOW_LIKES_EXPLAIN'	=> 'Mostrar en <code>viewtopic</code> el número de mensajes que han gustado a este usuario.',
 	'POSTLOVE_SHOW_LIKED'	=> 'Mostrar el número de mensajes que han gustado.',
