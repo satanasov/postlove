0ba1bd361781a898e0c809e4d1d5db3ef7d77a30
Stanislav Atanasov <lucifer@anavaro.com>
2014-08-18 08:21:49 +0300

    Small changes to css and tests

--- tests/functional/postlove_post_test.php
+++ tests/functional/postlove_post_test.php
@@ -28,13 +28,13 @@ class postlove_post_test extends postlove_base
 		$crawler = self::request('GET', "viewtopic.php?t={$post2['topic_id']}&sid={$this->sid}");
 		
 		//Do we see the static?
-		$this->assertContains('0x', $crawler->filter('#p' . $post2['post_id'])->filter('#postlove')->text());		
+		$this->assertContains('0 x', $crawler->filter('#p' . $post2['post_id'])->filter('#postlove')->text());		
 		
 		//togle like
 		//$crw1 = self::request('GET', 'app.php/postlove/togle/' . $post2['post_id'], array(), array(), array('CONTENT_TYPE'	=> 'application/json'));
 		
 		//reload page and test ...
 		$crawler = self::request('GET', "viewtopic.php?t={$post2['topic_id']}&sid={$this->sid}");
-		$this->assertContains('0x', $crawler->filter('#p' . $post2['post_id'])->filter('#postlove')->text());
+		$this->assertContains('0 x', $crawler->filter('#p' . $post2['post_id'])->filter('#postlove')->text());
 	}
 }
