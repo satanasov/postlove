a291f4f7c57e2d9104cc934d15e909f0604f18a6
Stanislav Atanasov <lucifer@anavaro.com>
2014-08-21 23:41:59 +0300

    Migrating to new notification type requests

--- ext.php
+++ ext.php
@@ -31,7 +31,7 @@ class ext extends \phpbb\extension\base
 
 				// Enable board rules notifications
 				$phpbb_notifications = $this->container->get('notification_manager');
-				$phpbb_notifications->enable_notifications('postlove');
+				$phpbb_notifications->enable_notifications('notification.type.postlove');
 				return 'notifications';
 
 			break;
@@ -59,7 +59,7 @@ class ext extends \phpbb\extension\base
 
 				// Disable board rules notifications
 				$phpbb_notifications = $this->container->get('notification_manager');
-				$phpbb_notifications->disable_notifications('postlove');
+				$phpbb_notifications->disable_notifications('notification.type.postlove');
 				return 'notifications';
 
 			break;
@@ -94,7 +94,7 @@ class ext extends \phpbb\extension\base
 				{
 					// Purge board rules notifications
 					$phpbb_notifications = $this->container->get('notification_manager');
-					$phpbb_notifications->purge_notifications('postlove');
+					$phpbb_notifications->purge_notifications('notification.type.postlove');
 				}
 				catch (\phpbb\notification\exception $e)
 				{
