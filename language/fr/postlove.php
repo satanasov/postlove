c7e8871a3bb26e1269b75a407d5d37809135eb4d
Raphaël <Galixte@users.noreply.github.com>
2015-02-11 00:25:25 +0100

    FRENCH translation for postlove.php file
    FRENCH translation for postlove.php file

---
+++ language/fr/postlove.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+*
+* @package Post Love [French]
+* @translated into French by Galixte (http://www.galixte.com)
+*
+* @copyright (c) 2014
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'POSTLOVE_USER_LIKES'	=> 'Nombre de “J’aime” partagés',
+	'POSTLOVE_USER_LIKED'	=> 'Nombre de “J’aime” reçus',
+
+	'NOTIFICATION_POSTLOVE_ADD'	=> '%1$s <b>aime</b> votre message "%2$s"',
+	'NOTIFICATION_TYPE_POST_LOVE'	=> 'Quelqu’un aime un de vos messages.',
+));
