33775b1d24eb7ba72cccfbe609e7411ee6f7329d
Raphaël M. <Galixte@users.noreply.github.com>
2015-08-26 16:43:08 +0200

    Update of FRENCH translation for Post Love extension
    Update for 1.1.0 version.

--- language/fr/info_acp_postlove.php
+++ language/fr/info_acp_postlove.php
@@ -1,30 +1,66 @@
 <?php
-
 /**
 *
-* @package Post Love [French]
-* @translated into French by Galixte (http://www.galixte.com)
+* Post Love extension for the phpBB Forum Software package.
+* French translation by Galixte (http://www.galixte.com)
 *
-* @copyright (c) 2014
+* @copyright (c) 2015 Stanislav Atanasov <http://anavaro.com>
 * @license GNU General Public License, version 2 (GPL-2.0)
 *
 */
 
+/**
+* DO NOT CHANGE
+*/
 if (!defined('IN_PHPBB'))
 {
 	exit;
 }
+
 if (empty($lang) || !is_array($lang))
 {
 	$lang = array();
 }
 
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
+
 $lang = array_merge($lang, array(
 	'POSTLOVE_CONTROL'	=> '« J’aime » un message',
-	'POSTLOVE_USE_CSS'	=> 'Utiliser le CSS fourni',
-	'POSTLOVE_USE_CSS_EXPLAIN'	=> 'Afin de personnaliser aisément l’extension POST LOVE il est possible de ne plus utiliser le CSS par défaut. Si vous souhaitez utiliser vos propres images, veuillez vous référer au fichier : <code>overall_header_head_append.html</code>.',
-	'POSTLOVE_SHOW_LIKES'	=> 'Afficher le nombre de « J’aime » que l’utilisateur a partagé',
-	'POSTLOVE_SHOW_LIKES_EXPLAIN'	=> 'Afficher dans <code>viewtopic</code> le nombre de messages que l’utilisateur a aimé, sous forme de « J’aime ».',
-	'POSTLOVE_SHOW_LIKED'	=> 'Afficher le nombre de « J’aime » pour les messages de l’utilisateur',
-	'POSTLOVE_SHOW_LIKED_EXPLAIN'	=> 'Afficher dans <code>viewtopic</code> le nombre de messages de l’utilisateur aimés par les autres, sous forme de « J’aime ».',
+	'POSTLOVE_USE_CSS'	=> 'Utiliser la feuille de style CSS fournie',
+	'POSTLOVE_USE_CSS_EXPLAIN'	=> 'Permet de personnaliser aisément l’extension « Post Love » pour remplacer la feuille de style CSS par défaut. Pour utiliser ses propres images, se référer au fichier : <code>overall_header_head_append.html</code>.',
+	'POSTLOVE_SHOW_LIKES'	=> 'Afficher le nombre de « J’aime » exprimés par l’utilisateur',
+	'POSTLOVE_SHOW_LIKES_EXPLAIN'	=> 'Permet d’afficher le nombre de messages appréciés par l’utilisateur sur les pages des sujets, <code>viewtopic</code>, au moyen du terme : « J’aime ».',
+	'POSTLOVE_SHOW_LIKED'	=> 'Afficher le nombre de « J’aime » reçus par les utilisateurs',
+	'POSTLOVE_SHOW_LIKED_EXPLAIN'	=> 'Permet d’afficher le nombre de messages appréciés par les autres utilisateurs sur les pages des sujets, <code>viewtopic</code>, au moyen du terme « J’aime ».',
+
+	//Version 1.1 langs
+	'ACP_POSTLOVE_GRP'	=> 'Aimer un message',
+	'ACP_POSTLOVE'	=> 'Aimer un message',
+	'POSTLOVE_EXPLAIN'	=> 'Depuis cette page il est possible de modifier les paramètres de l’extension « Post Love ».',
+	'CONFIRM_MESSAGE'	=> 'Les modifications ont été sauvegardées !<br><br><a href="%1$s">Retour</а>',
+	'POSTLOVE_CURRENT_THEME'	=> 'Thème actuel',
+	'THEME_NAME'	=> 'Nom du thème',
+	'THEME_AUTHOR'	=> 'Auteur du thème',
+	'THEME_DESCRIPTION'	=> 'Description du thème',
+	'THEME_SUPPORT_STYLES'	=> 'Styles supportés',
+	'THEME_PREVIEW'	=> 'Aperçu',
+	'POSTLOVE_CHOOSE_THEME' => 'Sélectionner un thème',
+
+	'POSTLOVE_NO_THEMES_INSTALLED'	=> 'Il n’y a aucun thème installé !<br>Merci d’en ajouter dans le répertoire : <i>$phpbb_root_path/ext/anavaro/postlove/themes</i>.',
+	'THEME_CHANGED'	=> 'Le thème a été modifié.',
+	'POSTLOVE_NO_WRITE_ACTION'	=> 'Les permissions en écriture ne sont pas correctes !<br>Merci d’attribuer des permissions en écriture sur le répertoire : <i>$phpbb_root_path/ext/anavaro/postlove/styles</i>, au moyen de la commande : CHMOD 777.',
 ));
