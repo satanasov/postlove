acf683f7da415bb656d16eb0d7fd14a81c599903
Stanislav Atanasov <lucifer@anavaro.com>
2017-08-05 11:22:27 +0300

    Fixing tests

--- tests/functional/postlove_post_test.php
+++ tests/functional/postlove_post_test.php
@@ -16,11 +16,43 @@ namespace anavaro\postlove\tests\functional;
 class postlove_post_test extends postlove_base
 {
 	protected $post2 = array();
+	public function test_post()
+	{
+		include('CssParser.php');
+		$parser = new CssParser();
+		$parser->load_file('/home/travis/build/phpBB3/phpBB/ext/anavaro/postlove/styles/all/theme/default.css');
+		$parser->parse();
+
+		$this->login();
+
+		// Test creating topic and post to test
+		$post = $this->create_topic(2, 'Test Topic 1', 'This is a test topic posted by the testing framework.');
+		$crawler = self::request('GET', "viewtopic.php?t={$post['topic_id']}&sid={$this->sid}");
+
+		$post2 = $this->create_post(2, $post['topic_id'], 'Re: Test Topic 1', 'This is a test [b]post[/b] posted by the testing framework.');
+		$crawler = self::request('GET', "viewtopic.php?t={$post2['topic_id']}&sid={$this->sid}");
+
+		//Do we see the static?
+		$class = $crawler->filter('#p' . $post2['post_id'])->filter('.postlove')->filter('span')->attr('class');
+
+		$this->assertContains('heart-red-16.png', $parser->parsed['main']['.' . $class]['background']);
+		$this->assertContains('0 x', $crawler->filter('#p' . $post2['post_id'])->filter('.postlove')->text());
+
+		//toggle like
+		$url = $crawler->filter('#p' . $post2['post_id'])->filter('.postlove')->filter('a')->attr('href');
+		$crw1 = self::request('GET', substr($url, 1), array(), array(), array('CONTENT_TYPE'	=> 'application/json'));
+
+		//reload page and test ...
+		$crawler = self::request('GET', "viewtopic.php?t={$post2['topic_id']}&sid={$this->sid}");
+		$class = $crawler->filter('#p' . $post2['post_id'])->filter('.postlove')->filter('span')->attr('class');
+
+		$this->logout();
+	}
 
 	public function test_guest_see_loves()
 	{
 		$crawler = self::request('GET', "viewtopic.php?t=2&sid={$this->sid}");
-		$this->assertContains('0 x', $crawler->filter('#p3')->filter('.postlove')->text());
+		$this->assertContains('1 x', $crawler->filter('#p3')->filter('.postlove')->text());
 	}
 	
 	public function test_guests_cannot_like()
