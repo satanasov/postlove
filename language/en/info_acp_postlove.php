e38e1191bd0e3325b4caa5741fd8a831d80729d7
ThE KuKa <thekuka@gmail.com>
2017-09-03 19:20:54 +0200

    Small changes
    UTF-8 Variant & spaces change

--- language/en/info_acp_postlove.php
+++ language/en/info_acp_postlove.php
@@ -25,8 +25,8 @@ $lang = array_merge($lang, array(
 	'POSTLOVE_USE_CSS_EXPLAIN'	=> 'For easier customisation of the POST LOVE extension you could stop it from loading the default CSS. If you want to use your own images, please refer to <code>overall_header_head_append.html</code>.',
 	'POSTLOVE_SHOW_LIKES'	=> 'Show number of posts this user has liked',
 	'POSTLOVE_SHOW_LIKES_EXPLAIN'	=> 'Show in  <code>viewtopic</code> the number of posts the user has liked.',
-	'POSTLOVE_SHOW_LIKED'	=> 'Show the number of liked user\'s posts',
-	'POSTLOVE_SHOW_LIKED_EXPLAIN'	=> 'Show in <code>viewtopic</code> how many of the user\'s posts have been liked by others.',
+	'POSTLOVE_SHOW_LIKED'	=> 'Show the number of liked user’s posts',
+	'POSTLOVE_SHOW_LIKED_EXPLAIN'	=> 'Show in <code>viewtopic</code> how many of the user’s posts have been liked by others.',
 
 	//Version 1.1 langs
 	'ACP_POSTLOVE_GRP'	=> 'Post Love',
@@ -43,7 +43,7 @@ $lang = array_merge($lang, array(
 
 	'POSTLOVE_NO_THEMES_INSTALLED'	=> 'There are no themes installed!<br>Please add them in <i>$phpbb_root_path/ext/anavaro/postlove/themes</i> folder',
 	'THEME_CHANGED'	=> 'Theme changed',
-	'POSTLOVE_NO_WRITE_ACTION'	=> 'No write acccess!<br>Please allow write access to<i> $phpbb_root_path/ext/anavaro/postlove/styles </i>folder',
+	'POSTLOVE_NO_WRITE_ACTION'	=> 'No write acccess!<br>Please allow write access to <i>$phpbb_root_path/ext/anavaro/postlove/styles</i> folder',
 
 	'POSTLOVE_AUTHOR_LIKE'	=> 'Author can like posts',
 	'POSTLOVE_AUTHOR_LIKE_EXPLAIN'	=> 'Can the author like his/her own posts or not',
