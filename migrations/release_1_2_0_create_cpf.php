8ebdab04081e552dfbd089ef33bb60a77d3792f6
v12mike <v12mike@ingenious.co.nz>
2018-10-15 07:28:55 +0200

    Changed post_love timestamp to TIMESTAMP type

--- migrations/release_1_2_0_create_cpf.php
+++ migrations/release_1_2_0_create_cpf.php
@@ -3,7 +3,7 @@
 *
 * Post Love extension for the phpBB Forum Software package.
 *
-* @copyright (c) 2014 Lucifer <http://www.anavaro.com>
+* @copyright (c) 2016 v12mike
 * @license GNU General Public License, version 2 (GPL-2.0)
 *
 */
---
+++ migrations/release_2_0_0.php
@@ -0,0 +1,52 @@
+<?php
+/**
+*
+* Post Love extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2018 v12mike
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+namespace anavaro\postlove\migrations;
+
+/**
+* Primary migration
+*/
+class release_2_0_0 extends \phpbb\db\migration\profilefield_base_migration
+{
+	static public function depends_on()
+	{
+		return array(
+			'\anavaro\postlove\migrations\release_1_2_0_create_cpf',
+		);
+	}
+
+
+	public function update_schema()
+	{
+		return array(
+			'change_columns' => array(
+				$this->table_prefix . 'posts_likes'	=> array(
+					'timestamp' => array('TIMESTAMP', 0 ),
+				),
+			),
+		);
+	}
+
+	public function revert_schema()
+	{
+		return array(
+			'change_columns' => array(
+				$this->table_prefix . 'posts_likes'	=> array(
+					'timestamp' => array('VCHAR:32', 0),
+				),
+			),
+		);
+	}
+
+	public function update_data()
+	{
+		return array();
+	}
+}
+
